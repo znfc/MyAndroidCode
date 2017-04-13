c37285804047a72ae2fae8f4ad5bb89a823eb5fc
znfc <1165103172lin@sina.com>
2017-04-13 16:05:55 +0800

    添加listview的点击事件和activity的点击事件，解决checkbox错乱问题
    Signed-off-by: znfc <1165103172lin@sina.com>

--- myrunintestview/src/main/java/linlin/com/myrunintestview/LVItemAdapter.java
+++ myrunintestview/src/main/java/linlin/com/myrunintestview/LVItemAdapter.java
@@ -6,20 +6,32 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zhaopenglin on 2017/4/13.
  */
 
-public class LVItemAdapter extends BaseAdapter {
+public class LVItemAdapter extends BaseAdapter implements CompoundButton.OnCheckedChangeListener
+,OnClickListener{
     private Context context;
     private List<ItemBean> list;
+    Map<Integer, Boolean> isCheckMap ;
+    private MCallback mCallback;
 
-    public LVItemAdapter(Context context, List<ItemBean> list) {
+    public LVItemAdapter(Context context, List<ItemBean> list, Map<Integer, Boolean> isCheckMap) {
         this.context = context;
         this.list = list;
+        this.isCheckMap = isCheckMap;
+    }
+
+    public void setmCallback(MCallback mCallback) {
+        this.mCallback = mCallback;
     }
 
     @Override
@@ -38,7 +50,7 @@ public class LVItemAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder = null;
         if(convertView == null){
             viewHolder = new ViewHolder();
@@ -49,13 +61,71 @@ public class LVItemAdapter extends BaseAdapter {
         }else {
             viewHolder = (ViewHolder)convertView.getTag();
         }
+
         viewHolder.checkBox.setText(list.get(position).getItemName());
         viewHolder.button.setVisibility(list.get(position).isVisable()?View.VISIBLE:View.INVISIBLE);
+
+        /**
+         * 添加了如下代码后解决了缓冲造成的checkbox选中错乱的问题
+         */
+        viewHolder.checkBox.setTag(position);
+        viewHolder.button.setTag(position);
+        if(isCheckMap != null && isCheckMap.containsKey(position)){
+            viewHolder.checkBox.setChecked(isCheckMap.get(position));
+        }else{
+            viewHolder.checkBox.setChecked(false);
+        }
+
+        viewHolder.checkBox.setOnCheckedChangeListener(this);
+        viewHolder.button.setOnClickListener(this);
+
         return convertView;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        int secectedID = Integer.parseInt(buttonView.getTag().toString());
+        if(isChecked) {
+            //将选中的放入hashmap中
+            isCheckMap.put(secectedID, isChecked);
+            Toast.makeText(context,"选中了："+secectedID ,Toast.LENGTH_SHORT).show();
+        } else {
+            //取消选中的则剔除
+            isCheckMap.remove(secectedID);
+            isCheckMap.put(secectedID, false);
+            Toast.makeText(context,"缓存触发了："+secectedID ,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /**
+     * 自定义接口，用于回调按钮点击事件到Activity
+     * @author Ivan Xu
+     * 2014-11-26
+     */
+     public interface MCallback {
+         void click(View v);
+     }
+
+    @Override
+    public void onClick(View v) {
+        mCallback.click(v);
     }
 
     private final class ViewHolder {
         CheckBox checkBox;
         Button button;
+    }
+
+    public void selectAll(){
+        for (int i = 0; i < isCheckMap.size(); i++){
+            isCheckMap.put(i,true);
+        }
+    }
+    public void selectNone(){
+        for (int i = 0; i < isCheckMap.size(); i++){
+//            isCheckMap.remove(i);
+            isCheckMap.put(i,false);
+        }
     }
 }
--- myrunintestview/src/main/java/linlin/com/myrunintestview/MainActivity.java
+++ myrunintestview/src/main/java/linlin/com/myrunintestview/MainActivity.java
@@ -2,23 +2,47 @@ package linlin.com.myrunintestview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements LVItemAdapter.MCallback,
+        View.OnClickListener{
 
     private List<ItemBean> list;
+
+    Map<Integer, Boolean> isCheckMap ;
+    LVItemAdapter itemAdapter;
     private ListView listView;
+
+    private Button selectAllBtn;
+    private Button selectNoneBtn;
+    private Button startTextBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        selectAllBtn = (Button) findViewById(R.id.selectall_btn);
+        selectNoneBtn = (Button) findViewById(R.id.selectnone_btn);
+        startTextBtn = (Button) findViewById(R.id.startBtn);
         initDate();
-        LVItemAdapter itemAdapter= new LVItemAdapter(this,list);
+        itemAdapter= new LVItemAdapter(this,list,isCheckMap);
+        itemAdapter.setmCallback(this);
         listView = (ListView) findViewById(R.id.my_Listview);
         listView.setAdapter(itemAdapter);
+
+        selectAllBtn.setOnClickListener(this);
+        selectNoneBtn.setOnClickListener(this);
+        startTextBtn.setOnClickListener(this);
 
     }
 
@@ -26,14 +50,52 @@ public class MainActivity extends AppCompatActivity {
      * 初始化数据
      */
     private void initDate() {
+        isCheckMap =  new HashMap<Integer, Boolean>();
         list = new ArrayList<ItemBean>();
         for (int i = 0 ;i <20;i++){
             list.add(new ItemBean("reboot",true));
+            isCheckMap.put(i,false);
+        }
+    }
+
+    @Override
+    public void click(View v) {
+        Toast.makeText(
+                MainActivity.this,
+                "listview的内部的按钮被点击了！，位置是-->" + v.getTag()
+                , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.selectall_btn :
+                itemAdapter.selectAll();
+                itemAdapter.notifyDataSetChanged();
+                break;
+            case R.id.selectnone_btn :
+                itemAdapter.selectNone();
+                itemAdapter.notifyDataSetChanged();
+                break;
+            case R.id.startBtn :
+                startTest();
+                break;
+        }
+    }
+
+    private void startTest() {
+        Log.i("zhao11","isCheckMap.size():"+isCheckMap.size());
+        for (int i = 0; i < isCheckMap.size(); i++ ){
+            Log.i("zhao11",i+"是："+isCheckMap.get(i));
+            if (isCheckMap.get(i) != null && isCheckMap.get(i)){
+                Toast.makeText(this,i+"被选中了！",Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
     /**
      * 目前存在的问题checkbox选中一个时候上下滑动listview时候回造成下边的也被选中，这个是由于用了listview的缓存机制
      * 造成的，目前只发现了这个问题。20170413
+     * 已经在LVItemAdapter里解决了.20170413
      */
 }
